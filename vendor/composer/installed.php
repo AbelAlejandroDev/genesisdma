e003cb9d7646d6e00b3bbf3519388d0cb0804bae
AbelAlejandroDev <pyschofaint@gmail.com>
2024-10-23 21:27:58 -0400

    Bug form guia-gratuita fixed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a308c1d7d16710c0df057898ba9b5d636543c0a6',
+        'reference' => '313de689e8c4739918cd2ae31772b5e47404324c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a308c1d7d16710c0df057898ba9b5d636543c0a6',
+            'reference' => '313de689e8c4739918cd2ae31772b5e47404324c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
